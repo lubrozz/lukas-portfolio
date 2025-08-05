c8ce1b841150ed02cdd7327a368c326e2b456f17
Lukas Østergaard <lukas.ostergaard@abacusmedicine.com>
2025-08-05 15:47:44 +0200

    styling in quickabout

--- src/components/QuickAbout.tsx
+++ src/components/QuickAbout.tsx
@@ -1,31 +1,30 @@
 import "../App.css";
 import profilePic from "../assets/profil-pic.png";
+import { SocialIcon } from "react-social-icons";
 
 const QuickAbout = () => {
   return (
     <>
-      <div className="polyBackground" style={{ border: "1px solid blue" }}>
+      <div className="polyBackground">
         <img
           src={profilePic}
           alt="profile"
           height={"200rem"}
           style={{ padding: "0.5rem" }}
         ></img>
-        <h2 className="h2" style={{ color: "white" }}>
-          Lukas Corlin Østergaard
-        </h2>
+        <h2 style={{ color: "white" }}>Lukas Corlin Østergaard</h2>
         <h6 style={{ color: "white" }}>Student Software Developer</h6>
-        <p style={{ color: "white" }}>contact logos (linkedIn, GitHub)</p>
-        <div
-          className="double-element"
-          style={{ border: "1px solid red", justifyContent: "center" }}
-        >
-          <div style={{ border: "1px solid red", padding: "0.25rem" }}>
+        <div>
+          <SocialIcon url="https://www.linkedin.com/in/lukas-corlin-oestergaard/" />
+          <SocialIcon url="https://github.com/lubrozz/" />
+        </div>
+        <div className="double-element" style={{ justifyContent: "center" }}>
+          <div style={{ padding: "0.25rem" }}>
             <button className="button-81" role="button">
               Download CV
             </button>
           </div>
-          <div style={{ border: "1px solid red", padding: "0.25rem" }}>
+          <div style={{ padding: "0.25rem" }}>
             <button className="button-81" role="button">
               Contact me
             </button>
