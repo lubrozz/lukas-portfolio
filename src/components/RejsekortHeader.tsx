723b3a4b480432f11ba30cdb5aa2dfee1b4ca6be
Lukas <luoe@itu.dk>
2025-09-11 20:24:41 +0200

    Purpose of rejsekort app added to page.

--- src/components/RejsekortHeader.tsx
+++ src/components/RejsekortHeader.tsx
@@ -1,6 +1,16 @@
 const RejsekortHeader = () => {
   return (
     <header>
+      <div className="text-box-shadowed">
+        This page was made for my turn in my dorm rooms <i>Tour de Chambre</i>{" "}
+        where the theme of the room was the Danish metro. I printed out some
+        check in/out stands and gave everyone a printed travel card to use. The
+        drink of the room was a layered shot of limoncello and sambuca to
+        represent the M1 metro line. On my TV I put on a video of a front-seat
+        ride of the M1 from start to finish as people were chatting. I then
+        opened up this page and when around to each person, tapped the "Scan
+        Rejsekort". If it came up red, they would have to take another shot.
+      </div>
       <h1>Rejsekort tjek</h1>
     </header>
   );
