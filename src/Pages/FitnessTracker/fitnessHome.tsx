d0b63d1bb16e2467f26598d93c27ea3bc9d41bf0
Lukas Østergaard <lukas.ostergaard@abacusmedicine.com>
2025-10-16 16:36:50 +0200

    Creating exercise groups based on DB data

--- src/Pages/FitnessTracker/fitnessHome.tsx
+++ src/Pages/FitnessTracker/fitnessHome.tsx
@@ -1,14 +1,26 @@
+import { Key } from "react";
 import BackToHomeButton from "../../components/BackToHomeButton";
 import ExerciseGroup from "../../components/FitnessPage/ExerciseGroup";
+import { API_BASE_URL } from "../../config";
 
 import FitnessLayout from "../../Layout/FitnessLayout";
+
+const allExerciseGroups = await fetch(`${API_BASE_URL}/api/groups`).then(
+  (res) => res.json()
+);
 
 const FitnessHome = () => {
   return (
     <>
       <div>
         <FitnessLayout headerText="Gym Workouts">
-          <ExerciseGroup title="Exercise group 1" />
+          <div>
+            {allExerciseGroups.map((group: { id: Key; name: string }) => (
+              <div key={group.id}>
+                <ExerciseGroup title={group.name} />
+              </div>
+            ))}
+          </div>
         </FitnessLayout>
       </div>
     </>
